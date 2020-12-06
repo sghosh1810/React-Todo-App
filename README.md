2a121cdb954bd5935f24986b351271198c49362b
Shounak Ghosh <shounakghosh@Shounaks-MacBook-Air.local>
2020-12-06 23:46:54 +0530

    First and probably the last commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Into React [TODO App]
+Followed tut from Traversy Media React Crash Course
+
---
+++ src/components/TodoItem.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+//rce tab to generate class based component
+export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            backgroundColor: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+        }
+    }
+    render() {
+        const { title,id,completed } = this.props.todo;
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input checked={completed} type='checkbox' onChange={this.props.markComplete.bind(this,id)} />
+                    {' '}
+                    {title}
+                    <button style={btnStyle} onClick={this.props.delTodo.bind(this,id)}>x</button>
+                </p>
+            </div>
+        )
+    }
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    backgroundColor: '#ff0000',
+    color:'white',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
+
+export default TodoItem
